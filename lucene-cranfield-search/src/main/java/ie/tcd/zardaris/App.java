41a8a35b4af8c8ab5c552437cda1703050115a55
saqmod <zardaris@tcd.ie>
2022-09-29 21:17:22 +0100

    Initial setup

---
+++ lucene-cranfield-search/src/main/java/ie/tcd/zardaris/App.java
@@ -0,0 +1,13 @@
+package ie.tcd.zardaris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
